df90691ceca22f38b5b0a5850d35a29c939d19d9
Won <onatewill42@gmail.com>
2022-09-18 19:14:39 -0300

    pequena alterações na saída de dados

--- interface.py
+++ interface.py
@@ -1,18 +1,27 @@
 from funcoes import Calculadora
+
+print('Seja Bem-vindo a v.1 da minha calculadora \n'
+      'nota: O primeiro valor de qualquer recurso será 0.\n')
+
 res = 0
-recurso = True
+print(f"Display: {res}\n")
+recurso = ''
+
 
 while recurso != 'sair':
-    recurso = input('Qual recurso deseja usar?'
+    recurso = input('Qual recurso deseja usar?\n'
                     'soma, subtração, multiplicação, divisão: ').strip().lower()
-    num1 = float(input('Digite um valor numérico: '))
-    if recurso == 'soma':
-        res = Calculadora.soma(res, num1)
-    elif recurso == 'subtração':
-        res = Calculadora.subtracao(res, num1)
-    elif recurso == 'multiplicação':
-        res = Calculadora.multiplicacao(res, num1)
-    elif recurso == 'divisão':
-        res = Calculadora.divisao(res, num1)
 
-    print(res)
+    if recurso in ['soma', 'subtração', 'multiplicação', 'divisão']:
+        num1 = float(input('Digite um valor numérico: '))
+        if recurso == 'soma':
+            res = Calculadora.soma(res, num1)
+        elif recurso == 'subtração':
+            res = Calculadora.subtracao(res, num1)
+        elif recurso == 'multiplicação':
+            res = Calculadora.multiplicacao(res, num1)
+        elif recurso == 'divisão':
+            res = Calculadora.divisao(res, num1)
+        print(f'-----------------------------------------------')
+        print(f"\nDisplay: {res}\n")
+        print(f'-----------------------------------------------')
